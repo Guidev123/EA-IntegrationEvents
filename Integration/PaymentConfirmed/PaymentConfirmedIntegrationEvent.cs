230edace738b9c6dc9628ecfed9dc351cbbcf551
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-01-23 22:52:45 -0300

    feat: novo event de pagamento de pedido

---
+++ Integration/PaymentConfirmed/PaymentConfirmedIntegrationEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace EA.IntegrationEvents.Integration.PaymentConfirmed;
+
+public class PaymentConfirmedIntegrationEvent : IntegrationEvent
+{
+    public PaymentConfirmedIntegrationEvent(bool isPaid, string orderCode)
+    {
+        IsPaid = isPaid;
+        OrderCode = orderCode;
+    }
+
+    public bool IsPaid { get; private set; }
+    public string OrderCode { get; private set; }
+}
