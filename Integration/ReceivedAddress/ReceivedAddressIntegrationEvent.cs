27ee9838b761eeb3af0b8ddceeb5facfaa1a7cdc
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-01-30 19:14:00 -0300

    feat: event para adicionar endereco

---
+++ Integration/ReceivedAddress/ReceivedAddressIntegrationEvent.cs
@@ -0,0 +1,24 @@
+﻿namespace EA.IntegrationEvents.Integration.ReceivedAddress
+{
+    public class ReceivedAddressIntegrationEvent : IntegrationEvent
+    {
+        public ReceivedAddressIntegrationEvent(string street, string number, string additionalInfo, string neighborhood, string zipCode, string city, string state)
+        {
+            Street = street;
+            Number = number;
+            AdditionalInfo = additionalInfo;
+            Neighborhood = neighborhood;
+            ZipCode = zipCode;
+            City = city;
+            State = state;
+        }
+
+        public string Street { get; private set; }
+        public string Number { get; private set; }
+        public string AdditionalInfo { get; private set; }
+        public string Neighborhood { get; private set; }
+        public string ZipCode { get; private set; }
+        public string City { get; private set; }
+        public string State { get; private set; }
+    }
+}
